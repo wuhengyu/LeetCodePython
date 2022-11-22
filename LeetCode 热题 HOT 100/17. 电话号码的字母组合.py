e02d1ccdc62352d7c1dbd36ca1b17434ff5f8db2
wuhengyu <578060214@qq.com>
2022-11-22 10:44:58 +0800

    updates

---
+++ LeetCode 热题 HOT 100/17. 电话号码的字母组合.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/21 15:02
+# @Author  : Walter
+# @File    : 17. 电话号码的字母组合.py
+# @License : (C)Copyright Walter
+# @Desc    :
+'''
+给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+2 abc
+3 def
+4 ghi
+5 jkl
+6 mno
+7 pqrs
+8 tuv
+9 wxyz
+
+示例 1：
+输入：digits = "23"
+输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+示例 2：
+输入：digits = ""
+输出：[]
+
+示例 3：
+输入：digits = "2"
+输出：["a","b","c"]
+'''
+import itertools
+from typing import List
+
+
+class Solution1:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        combinations = list()
+        backtrack(0)
+        return combinations
+
+
+class Solution2:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        groups = (phoneMap[digit] for digit in digits)
+        return ["".join(combination) for combination in itertools.product(*groups)]
+
+
+print(Solution1().letterCombinations('23'))
+print(Solution2().letterCombinations('23'))
