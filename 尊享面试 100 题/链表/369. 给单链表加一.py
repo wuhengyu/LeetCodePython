25bdf8d680ee3123497ca8694e2b9f0c65353961
wuhengyu <578060214@qq.com>
2023-06-09 18:16:19 +0800

    add files

--- 尊享面试 100 题/链表/369. 给单链表加一.py
+++ 尊享面试 100 题/链表/369. 给单链表加一.py
@@ -47,19 +47,25 @@ class ListNode:
 # 方法一：哨兵头节点
 class Solution:
     def plusOne(self, head: ListNode) -> ListNode:
+        # 哨兵节点
         sentinel = ListNode(0)
+        # 加上哨兵头结点
         sentinel.next = head
+        # 记录最后一个不为9的节点
         not_nine = sentinel
 
+        # 不是9节点，找到最靠右的不是9的节点提取出来
         while head:
             if head.val != 9:
-                # 没有9节点的时候，不需要哨兵结点
+                # 记录最后一个不为9的节点
                 not_nine = head
             head = head.next
 
+        # 最右的数字相做加运算
         not_nine.val += 1
         not_nine = not_nine.next
 
+        # 将最右边的数字之后的所有9变成0，不是9的数字不变，not_nine等于None，可以直接跳过
         while not_nine:
             not_nine.val = 0
             not_nine = not_nine.next
@@ -81,14 +87,37 @@ singleList = Solution()
 # while root1:
 #     print(root1.val)
 #     root1 = root1.next
+#
+#
+# root2 = ListNode(9)
+# root2.next = ListNode(9)
+# root2.next.next = ListNode(9)
+# root2 = singleList.plusOne(root2)
+#
+# # 输出链表
+# while root2:
+#     print(root2.val)
+#     root2 = root2.next
 
 
-root2 = ListNode(9)
-root2.next = ListNode(9)
-root2.next.next = ListNode(9)
-root2 = singleList.plusOne(root2)
+root3 = ListNode(1)
+root3.next = ListNode(2)
+root3.next.next = ListNode(9)
+root3 = singleList.plusOne(root3)
 
 # 输出链表
-while root2:
-    print(root2.val)
-    root2 = root2.next
+while root3:
+    print(root3.val)
+    root3 = root3.next
+
+
+root4 = ListNode(1)
+root4.next = ListNode(2)
+root4.next.next = ListNode(9)
+root4.next.next.next = ListNode(1)
+root4 = singleList.plusOne(root4)
+
+# 输出链表
+while root4:
+    print(root4.val)
+    root3 = root4.next
